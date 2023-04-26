bf1bfd12694b41ca6949f5a5d0bddaf09c2495ae
gishk <124793197+gishk@users.noreply.github.com>
2023-04-26 17:34:26 +0300

    Add files via upload

---
+++ program-main/main.py
@@ -0,0 +1,81 @@
+print('Загрузка программы...') 
+import telebot 
+from telebot import types 
+import sqlite3
+con = sqlite3.connect("data.db")
+cursor = con.cursor()
+bot = telebot.TeleBot('6292695080:AAGt_t9K2DMIUnaKEVFcDQuUPDSAOTSxL1k')  
+print('Бот запущен!') 
+ 
+def controler_keyboard(name_kb): 
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True) 
+    if name_kb == 'main':
+        b1 = types.KeyboardButton('Популярные фильмы') 
+        b2 = types.KeyboardButton('Найти фильм') 
+        markup.add(b1) 
+        markup.add(b2)
+    if name_kb == 'pf': 
+        b1 = types.KeyboardButton('Жанры') 
+        b2 = types.KeyboardButton('Новинки')
+        b3 = types.KeyboardButton('Назад')
+        markup.add(b1) 
+        markup.add(b2)
+        markup.add(b3)
+    if name_kb == 'look': 
+        b1 = types.KeyboardButton('По названию')
+        b2 = types.KeyboardButton('По году') 
+        b3 = types.KeyboardButton('Назад')
+        markup.add(b1) 
+        markup.add(b2)
+        markup.add(b3)
+    if name_kb == 'ok': 
+        b1 = types.KeyboardButton('Предполагаемый фильм')
+        b2 = types.KeyboardButton('Назад')
+        markup.add(b1) 
+        markup.add(b2)
+    if name_kb == 'k': 
+        b1 = types.KeyboardButton('Предполагаемый фильм')
+        b2 = types.KeyboardButton('Назад')
+        markup.add(b1) 
+        markup.add(b2)
+    return markup 
+
+def controler_keyboard_2(name_kb):
+    markup = types.InlineKeyboardMarkup()
+    if name_kb == 'pf_1':
+        b1 = types.InlineKeyboardButton(text='Боевик')
+        b2 = types.InlineKeyboardButton(text='Ужасы')
+        markup.add(b1, b2)
+    return markup
+
+def title(message):
+    return False
+
+@bot.message_handler(commands=['start']) 
+def start(message): 
+    uid = message.chat.id 
+    name = message.from_user.first_name 
+
+    bot.send_message(uid, 'Добро пожаловать!', reply_markup=controler_keyboard('main'))
+
+@bot.message_handler(content_types=['text']) 
+def main_controler(message): 
+    uid = message.chat.id 
+    name = message.from_user.first_name 
+    
+    if message.text == 'Популярные фильмы':
+        bot.send_message(uid, 'Каким способом вы хотите найти фильм?', reply_markup=controler_keyboard('pf'))
+
+    if message.text == 'Жанры':
+        bot.send_message(uid, 'Вот список жанров:', reply_markup=controler_keyboard_2('pf_1'))
+
+    if message.text == 'Найти фильм':
+        bot.send_message(uid, 'Как вы хотите найти фильм?', reply_markup=controler_keyboard('look'))
+    if message.text == 'По названию':
+        bot.send_message(uid, 'Введите текст поиска...')
+        bot.register_next_step_handler(message, title)
+    if message.text == 'По году':
+        bot.send_message(uid, 'Введите текст поиска...')
+        bot.register_next_step_handler(message, title)
+   
+bot.infinity_polling()
